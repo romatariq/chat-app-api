10bf035041ae6363df027a64cfda5b827d7e2d0b
Roma Tariq <roma.imran@gmail.com>
2023-09-24 14:13:52 +0300

    Add docker-compose

--- App.DAL.EF/AppDbContextFactory.cs
+++ App.DAL.EF/AppDbContextFactory.cs
@@ -5,12 +5,11 @@ namespace App.DAL.EF;
 
 public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
-    private const string ConnectionString = "Host=localhost:5445;Database=flight-app;Username=postgres;Password=postgres";
     
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseNpgsql(ConnectionString);
+        optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection"));
 
         return new AppDbContext(optionsBuilder.Options);
     }
