cca2f465fbac6c274d766b9e7f7caf7cdeafb97f
Roma Tariq <roma.imran@gmail.com>
2023-10-23 17:13:30 +0300

    Refactor seeding configuration

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,5 +1,4 @@
 using App.DAL.EF;
-using App.DAL.EF.Seeding;
 using App.Domain.Identity;
 using Asp.Versioning;
 using Asp.Versioning.ApiExplorer;
@@ -89,7 +88,7 @@ builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true
 var app = builder.Build();
 
 // Set up db
-SetupDb(app, app.Environment, app.Configuration);
+SeedingRunner.SetupDb(app, app.Configuration);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -128,76 +127,3 @@ app.UseSwaggerUI(options =>
 
 
 app.Run();
-
-
-
-
-void SetupDb(IApplicationBuilder webApp, IWebHostEnvironment appEnvironment, IConfiguration appConfiguration)
-{
-    using var serviceScope = webApp.ApplicationServices
-        .GetRequiredService<IServiceScopeFactory>()
-        .CreateScope();
-    using var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
-
-
-    if (context == null)
-    {
-        throw new ApplicationException("Problem in services. Can't initialize Application DB Context");
-    }
-
-    using var userManager = serviceScope.ServiceProvider.GetService<UserManager<AppUser>>();
-    using var roleManager = serviceScope.ServiceProvider.GetService<RoleManager<AppRole>>();
-    
-    if (userManager == null || roleManager == null)
-    {
-        throw new ApplicationException("Problem in services. Can't initialize UserManager or RoleManager");
-    }
-    
-    var logger = serviceScope.ServiceProvider.GetService<ILogger<IApplicationBuilder>>();
-    if (logger == null)
-    {
-        throw new ApplicationException("Problem in services. Can't initialize logger");
-    }
-
-    if (context.Database.ProviderName != null && context.Database.ProviderName.Contains("InMemory"))
-    {
-        return;
-    }
-
-    // wait for db connection
-    var startedAt = DateTime.UtcNow;
-    var isDbConnectable = context.Database.CanConnectAsync().Result;
-    while (!isDbConnectable)
-    {
-        isDbConnectable = context.Database.CanConnectAsync().Result;
-        if (!isDbConnectable && (DateTime.UtcNow - startedAt).Seconds > 10)
-        {
-            throw new ApplicationException("Could not connect to database");
-        }
-    }
-    
-
-    if (appConfiguration.GetValue<bool>("InitializeData:DropDatabase"))
-    {
-        logger.LogWarning("Dropping database");
-        DbInitializer.DropDatabase(context);
-    }
-
-    if (appConfiguration.GetValue<bool>("InitializeData:MigrateDatabase"))
-    {
-        logger.LogInformation("Migrating database");
-        DbInitializer.MigrateDatabase(context);
-    }
-
-    if (appConfiguration.GetValue<bool>("InitializeData:SeedIdentity"))
-    {
-        logger.LogInformation("Seeding identity");
-        DbInitializer.SeedIdentity(userManager, roleManager);
-    }
-
-    if (appConfiguration.GetValue<bool>("InitializeData:SeedData"))
-    {
-        logger.LogInformation("Seeding initial app data");
-    }
-
-}
