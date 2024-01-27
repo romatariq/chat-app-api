18a0c5ac2d4dd48ed0cbb42b153ecd352f3f1377
Roma Tariq <roma.imran@gmail.com>
2024-01-27 18:49:09 +0200

    Refactor - add separate file for devSeedData

--- App.DAL.EF/Seeding/DbInitializer.cs
+++ App.DAL.EF/Seeding/DbInitializer.cs
@@ -33,10 +33,10 @@ public static class DbInitializer
 
     public static async Task SeedDevData(AppDbContext ctx)
     {
-        await SeedDevUrls(ctx);
+        await DevDataInitializer.SeedDevUrls(ctx);
         await ctx.SaveChangesAsync();
         
-        await SeedDevComments(ctx);
+        await DevDataInitializer.SeedDevComments(ctx);
         await ctx.SaveChangesAsync();
     }
 
@@ -103,70 +103,5 @@ public static class DbInitializer
             .ToListAsync();
 
         await ctx.GroupUsers.AddRangeAsync(groupUsers);
-    }
-
-    private static async Task SeedDevUrls(AppDbContext ctx)
-    {
-        if (await ctx.WebDomains.AnyAsync()) return;
-        var domain = new WebDomain
-        {
-            Name = "localhost:3000"
-        };
-        await ctx.WebDomains.AddAsync(domain);
-
-        if (await ctx.Urls.AnyAsync()) return;
-        var url = new Url
-        {
-            WebDomainId = domain.Id
-        };
-        await ctx.Urls.AddAsync(url);
-    }
-    
-    
-    private static async Task SeedDevComments(AppDbContext ctx)
-    {
-        if (await ctx.Comments.AnyAsync()) return;
-
-        var allChatId = await ctx.Groups
-            .Where(g => g.GroupType == EGroupType.All)
-            .Select(g => g.Id)
-            .SingleAsync();
-
-        var adminId = await ctx.Users
-            .Where(u => u.UserName!.ToLower() == "admin")
-            .Select(u => u.Id)
-            .SingleAsync();
-
-        var urlId = await ctx.Urls
-            .Include(u => u.WebDomain)
-            .Where(u => u.WebDomain!.Name == "localhost:3000" && u.Path == null)
-            .Select(u => u.Id)
-            .SingleAsync();
-
-        var random = new Random();
-        var comments = new List<Comment>();
-        for (int i = 0; i < 20; i++)
-        {
-            comments.Add(new Comment()
-            {
-                UserId = adminId,
-                GroupId = allChatId,
-                UrlId = urlId,
-                CreatedAtUtc = DateTime.UtcNow,
-                Text =
-                    "this is a comment 2 very long comment agdsad sa das das dsa dsa saddadasdsa f sadasudhoipaufgousaygfoasadasudhoipaufgousaygfoasadasudhoipaufgousaygfoa dhoipaufgousaygfo asadasud hoipau fgous aygfodhoipa ufgousaygfoasada sudhoipaufgousaygfodhoipaufgousaygfoasadasudhoi paufgousaygfodhoipaufgousaygfoasadasudhoipaufgousaygfo",
-                CommentReactions = random.Next(10) < 5 ? null : new List<CommentReaction>()
-                {
-                    new()
-                    {
-                        UserId = adminId,
-                        ReactionType = random.Next(10) < 5 ? ECommentReactionType.Like : ECommentReactionType.Dislike,
-                        CreatedAtUtc = DateTime.UtcNow
-                    } 
-                },
-            });
-        }
-
-        await ctx.Comments.AddRangeAsync(comments);
     }
 }
---
+++ App.DAL.EF/Seeding/DevDataInitializer.cs
@@ -0,0 +1,73 @@
+using App.Domain;
+using App.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.DAL.EF.Seeding;
+
+internal static class DevDataInitializer
+{
+    public static async Task SeedDevUrls(AppDbContext ctx)
+    {
+        if (await ctx.WebDomains.AnyAsync()) return;
+        var domain = new WebDomain
+        {
+            Name = "localhost:3000"
+        };
+        await ctx.WebDomains.AddAsync(domain);
+
+        if (await ctx.Urls.AnyAsync()) return;
+        var url = new Url
+        {
+            WebDomainId = domain.Id
+        };
+        await ctx.Urls.AddAsync(url);
+    }
+    
+    
+    public static async Task SeedDevComments(AppDbContext ctx)
+    {
+        if (await ctx.Comments.AnyAsync()) return;
+
+        var allChatId = await ctx.Groups
+            .Where(g => g.GroupType == EGroupType.All)
+            .Select(g => g.Id)
+            .SingleAsync();
+
+        var adminId = await ctx.Users
+            .Where(u => u.UserName!.ToLower() == "admin")
+            .Select(u => u.Id)
+            .SingleAsync();
+
+        var urlId = await ctx.Urls
+            .Include(u => u.WebDomain)
+            .Where(u => u.WebDomain!.Name == "localhost:3000" && u.Path == null)
+            .Select(u => u.Id)
+            .SingleAsync();
+
+        var random = new Random();
+        var comments = new List<Comment>();
+        for (int i = 0; i < 20; i++)
+        {
+            comments.Add(new Comment()
+            {
+                UserId = adminId,
+                GroupId = allChatId,
+                UrlId = urlId,
+                CreatedAtUtc = DateTime.UtcNow,
+                Text =
+                    "this is a comment 2 very long comment agdsad sa das das dsa dsa saddadasdsa f sadasudhoipaufgousaygfoasadasudhoipaufgousaygfoasadasudhoipaufgousaygfoa dhoipaufgousaygfo asadasud hoipau fgous aygfodhoipa ufgousaygfoasada sudhoipaufgousaygfodhoipaufgousaygfoasadasudhoi paufgousaygfodhoipaufgousaygfoasadasudhoipaufgousaygfo",
+                CommentReactions = random.Next(10) < 5 ? null : new List<CommentReaction>()
+                {
+                    new()
+                    {
+                        UserId = adminId,
+                        ReactionType = random.Next(10) < 5 ? ECommentReactionType.Like : ECommentReactionType.Dislike,
+                        CreatedAtUtc = DateTime.UtcNow
+                    } 
+                },
+            });
+        }
+
+        await ctx.Comments.AddRangeAsync(comments);
+    }
+}
